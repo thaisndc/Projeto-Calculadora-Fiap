bab4d82243204af7cd9f11f526456f1349881097
Thais Nascimento <thais.12_@hotmail.com>
2022-09-16 11:07:21 -0300

    Foi adicionado um arquivo calculadora

---
+++ src/calculadora.ts
@@ -0,0 +1,135 @@
+console.log("------- Tabuada do 1 --------")
+
+
+
+for(let x: number = 0; x <= 10; x++) {
+
+    let conta = x * 1;
+
+    console.log(`2 x ${x} = ${conta}`);
+
+}
+
+
+
+console.log("------- Tabuada do 2 --------")
+
+
+
+for(let x: number = 0; x <= 10; x++) {
+
+    let conta = x * 2;
+
+    console.log(`2 x ${x} = ${conta}`);
+
+}
+
+
+
+console.log("------- Tabuada do 3 --------")
+
+
+
+for(let x: number = 0; x <= 10; x++) {
+
+    let conta = x * 3;
+
+    console.log(`2 x ${x} = ${conta}`);
+
+}
+
+
+
+console.log("------- Tabuada do 4 --------")
+
+
+
+for(let x: number = 0; x <= 10; x++) {
+
+    let conta = x * 4;
+
+    console.log(`2 x ${x} = ${conta}`);
+
+}
+
+
+
+console.log("------- Tabuada do 5 --------")
+
+
+
+for(let x: number = 0; x <= 10; x++) {
+
+    let conta = x * 5;
+
+    console.log(`2 x ${x} = ${conta}`);
+
+}
+
+
+
+console.log("------- Tabuada do 6 --------")
+
+
+
+for(let x: number = 0; x <= 10; x++) {
+
+    let conta = x * 6;
+
+    console.log(`2 x ${x} = ${conta}`);
+
+}
+
+
+
+console.log("------- Tabuada do 7 --------")
+
+
+
+for(let x: number = 0; x <= 10; x++) {
+
+    let conta = x * 7;
+
+    console.log(`2 x ${x} = ${conta}`);
+
+}
+
+console.log("------- Tabuada do 8 --------")
+
+
+
+for(let x: number = 0; x <= 10; x++) {
+
+    let conta = x * 8;
+
+    console.log(`2 x ${x} = ${conta}`);
+
+}
+
+
+
+console.log("------- Tabuada do 9 --------")
+
+
+
+for(let x: number = 0; x <= 10; x++) {
+
+    let conta = x * 9;
+
+    console.log(`2 x ${x} = ${conta}`);
+
+}
+
+
+
+console.log("------- Tabuada do 10 --------")
+
+
+
+for(let x: number = 0; x <= 10; x++) {
+
+    let conta = x * 10;
+
+    console.log(`2 x ${x} = ${conta}`);
+
+}
